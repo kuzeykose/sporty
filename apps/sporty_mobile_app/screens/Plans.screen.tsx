e8d2dd03fa149a91124a9da1fab477e538ba55c9
kuzeykose <kosekuzey97@gmail.com>
2023-05-19 10:18:09 -0400

    Mobile app remove for new version

--- apps/sporty_mobile_app/screens/Plans.screen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function PlansScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
