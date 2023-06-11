189b76f669ba1fc62f2c5ff9bc9a1acc021a04b9
kuzeykose <kuzeykose>
2023-06-11 15:14:45 -0400

    Remove extra imports

--- apps/server/routes/plan.routes.ts
+++ apps/server/routes/plan.routes.ts
@@ -1,9 +1,6 @@
-import { Express, Request, Response } from 'express';
+import { Express } from 'express';
 import { create, list } from '../contollers/plan.controller';
 import verify from '../middlewares/jwt';
-import { v4 as uuidv4 } from 'uuid';
-import jwt from 'jsonwebtoken';
-import { ddb, s3 } from '../aws';
 
 const Plan = (app: Express) => {
   app.post('/api/plan/create', [verify.verifyToken, verify.isAdmin], create);
