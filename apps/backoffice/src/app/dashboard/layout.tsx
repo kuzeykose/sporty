107373c3144cbcebfcf158d0700c2d75c0d3f39f
aberkayk <aberkayk@gmail.com>
2023-08-03 16:34:33 +0300

    small-sidebar created

--- apps/backoffice/src/app/dashboard/layout.tsx
+++ apps/backoffice/src/app/dashboard/layout.tsx
@@ -1,20 +1,28 @@
 'use client';
 
 import { LargeSidebar } from '@/components/large-sidebar';
+import { SmallSidebar } from '@/components/small-sidebar';
 import { Button } from '@/components/ui/button';
 import { playlists } from '@/data/playlist';
 import { BellIcon } from '@radix-ui/react-icons';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 import React from 'react';
 
 export default function DashboardLayout({ children }: any) {
   const pathname = usePathname();
+  const params = useParams();
+
+  console.log('params:', params);
 
   console.log('pathname:', pathname.split('/'));
 
   return (
     <div className="flex h-full">
-      <LargeSidebar playlists={playlists} className="h-screen flex flex-col justify-between min-w-fit" />
+      {params.programId ? (
+        <SmallSidebar playlists={playlists} className="h-screen flex flex-col justify-between min-w-fit" />
+      ) : (
+        <LargeSidebar playlists={playlists} className="h-screen flex flex-col justify-between min-w-fit" />
+      )}
       <div className="flex flex-col w-full">
         <div className="h-12 w-full border-b text-sm flex items-center justify-between px-7">
           <h2 className="text-sm tracking-tight capitalize">sporty {pathname.split('/').join(' / ')}</h2>
--- apps/backoffice/src/components/small-sidebar.tsx
+++ apps/backoffice/src/components/small-sidebar.tsx
@@ -8,39 +8,30 @@ interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   playlists: Playlist[];
 }
 
-export function SmallSidebar({ className, playlists }: SidebarProps) {
+export function SmallSidebar({ className }: SidebarProps) {
   return (
     <div className={cn('border-r', className)}>
       <div className="space-y-4">
         <div className="border-b h-12 flex items-center">
           <h2 className="px-7 text-lg font-semibold tracking-tight">Dashboard</h2>
         </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Projects</h2>
-          <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start gap-2">
-              <DashboardIcon className="h-4 w-4" />
-              All Projects
-            </Button>
-          </div>
+        <div className="space-y-1 px-2 rounded-md">
+          <Button variant="secondary" className="w-full justify-start gap-2">
+            <DashboardIcon className="h-4 w-4" />
+            All Projects
+          </Button>
         </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Account</h2>
-          <div className="space-y-1">
-            <Button variant="ghost" className="w-full justify-start gap-2">
-              <PersonIcon className="h-4 w-4" />
-              Preferences
-            </Button>
-          </div>
+        <div className="space-y-1 px-2 rounded-md">
+          <Button variant="ghost" className="w-full justify-start gap-2">
+            <PersonIcon className="h-4 w-4" />
+            Preferences
+          </Button>
         </div>
-        <div className="px-3 py-2">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Documentation</h2>
-          <div className="space-y-1">
-            <Button variant="ghost" className="w-full justify-start gap-2">
-              <Link2Icon className="h-4 w-4" />
-              How to use Sporty
-            </Button>
-          </div>
+        <div className="space-y-1 px-2 rounded-md">
+          <Button variant="ghost" className="w-full justify-start gap-2">
+            <Link2Icon className="h-4 w-4" />
+            How to use Sporty
+          </Button>
         </div>
       </div>
       <div className="px-3 py-2 border-t">
