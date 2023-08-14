e3a995d04be6051e70d9f509c9054359af1e86f2
aberkayk <aberkayk@gmail.com>
2023-08-14 12:33:11 +0300

    Add edit buttons and input to program settings page. Button onClicks not working.

--- apps/backoffice/src/app/dashboard/programs/[programId]/settings/page.tsx
+++ apps/backoffice/src/app/dashboard/programs/[programId]/settings/page.tsx
@@ -1,9 +1,7 @@
+import { Button } from '@/components/ui/button';
 import { ProgramSettings } from '@/constants/Programs.type';
 import { getProgram } from '@/services/programs';
-import { Separator } from '@radix-ui/react-separator';
-
 import React from 'react';
-import { Form } from 'react-hook-form';
 
 type ProgramSettingsParams = {
   params: {
@@ -20,17 +18,27 @@ export default async function ProgramSettings({ params }: ProgramSettingsParams)
         <div className=" pb-4">
           <h2 className=" text-2xl font-extrabold">Program Settings</h2>
         </div>
-        <div className="space-y-4">
+        <div className=" space-y-4">
           <div className="border-b border-b-gray-300 pb-4">
             <h4 className=" text-lg font-bold">Information</h4>
           </div>
           <div className=" border-b border-b-gray-100 pb-4 grid grid-cols-4">
-            <p className=" col-span-1 text-sm font-bold leading-6">Name</p>
-            <p className=" col-span-3 text-sm font-medium leading-6">{program.name}</p>
+            <p className=" col-span-1 text-sm font-bold leading-6 flex items-center">Name</p>
+            <div className="col-span-3 flex justify-between items-center">
+              <input className="text-sm font-medium leading-6" type="text" value={program.name} disabled />
+              <Button size="icon" className="h-6 w-14">
+                Edit
+              </Button>
+            </div>
           </div>
           <div className=" border-b border-b-gray-100 pb-4 grid grid-cols-4">
-            <p className=" col-span-1 text-sm font-bold leading-6">Description</p>
-            <p className=" col-span-3 text-sm font-medium leading-6">{program.description}</p>
+            <p className=" col-span-1 text-sm font-bold leading-6 flex items-center">Description</p>
+            <div className="col-span-3 flex justify-between items-center">
+              <input className="text-sm font-medium leading-6" type="text" value={program.description} disabled />
+              <Button size="icon" className="h-6 w-14">
+                Edit
+              </Button>
+            </div>
           </div>
           <div className=" border-b border-b-gray-100 pb-4 grid grid-cols-4">
             <p className=" col-span-1 text-sm font-bold leading-6">Status</p>
